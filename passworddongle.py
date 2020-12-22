b544c0071a25836a8e2726d6cc1ad2fe38448643
Bob Harold <bgithub@halfgoat.com>
2020-12-21 19:55:46 -0500

    remove gc free prints

--- passworddongle.py
+++ passworddongle.py
@@ -24,19 +24,19 @@ touch4 = touchio.TouchIn(board.D4)  # yellow, dot
 buttons = [touch4, touch3, touch1]
 
 
-print("free before:",gc.mem_free())
+#print("free before:",gc.mem_free())
 import morseword
-print("free after morseword import:",gc.mem_free())
+#print("free after morseword import:",gc.mem_free())
 key=morseword.morseword(buttons)
-print("key:",key)
+#print("key:",key)
 del sys.modules[morseword.__name__]
 del morseword
-print("free after morseword:",gc.mem_free())
+#print("free after morseword:",gc.mem_free())
 
 
 gc.collect()
-print("free after gc:",gc.mem_free())
+#print("free after gc:",gc.mem_free())
 import pwmenu
-print("free after pwmenu import:",gc.mem_free())
+#print("free after pwmenu import:",gc.mem_free())
 pwmenu.menu(key, buttons)
 #print("free after pwmenu:",gc.mem_free())
